391b684d3f4db3ed114fe0d1fad50f4cadb5d216
Nezquik_lap <you@example.com>
2020-07-13 16:38:55 +0500

    add Photon

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,91 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "1";
+    [SerializeField] private Text logText;
+    [SerializeField] private Text playerName;
+    [SerializeField] private Text playerList;
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 100);
+        playerName.text = "You are " + PhotonNetwork.NickName;
+        UpdatePlayerList();
+        Connect();
+    }
+
+    private void UpdatePlayerList()
+    {
+        playerList.text = "Player list: \n";
+        foreach (var player in PhotonNetwork.PlayerList)
+        {
+            playerList.text += player.NickName + "\n";
+        }
+    }
+
+    #region Public Methods
+
+    /// <summary>
+    /// Start the connection process.
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    ///
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
+
+    private void Log(string mess)
+    {
+        Debug.Log(mess);
+        logText.text += mess + "\n";
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Log("ConnectedToMaster");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Log(PhotonNetwork.NickName + "connected to Room");
+        UpdatePlayerList();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Log("No random room available, so we create one");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Log(newPlayer.NickName + " entered to the Room");
+        UpdatePlayerList();
+    }
+}
